deef91f7f90ba92708a7229321e0a820a9169a2b
kevin <kemen95@hotmail.com>
2025-10-27 13:12:35 -0600

    WIP

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,13 @@ const config: Config.InitialOptions = {
   collectCoverage: true,
   testPathIgnorePatterns: ['/node_modules'],
   transform: {
-    '^.+\\.ts?$': 'ts-jest'
+    '^.+\\.ts?$': ['ts-jest', {
+      tsconfig: {
+        paths: {
+          '@review/*': ['src/*']
+        }
+      }
+    }]
   },
   testMatch: ['<rootDir>/src/**/test/*.ts'],
   collectCoverageFrom: ['src/**/*.ts', '!src/**/test/*.ts?(x)', '!**/node_modules/**'],
@@ -22,7 +28,8 @@ const config: Config.InitialOptions = {
   },
   coverageReporters: ['text-summary', 'lcov'],
   moduleNameMapper: {
-    '@auth/(.*)': ['<rootDir>/src/$1']
+    '^@review/(.*)$': '<rootDir>/src/$1',
+    '^@auth/(.*)$': '<rootDir>/src/$1'
   }
 };
 
